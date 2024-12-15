eda42110dd1bc653250b9ce6d791c213913a321a
pablok <pablok@com>
2024-12-15 12:06:21 +0300

    add network module

---
+++ network/src/main/java/com/pablok/kinopoisklight/network/dto/MovieDto.kt
@@ -0,0 +1,25 @@
+package com.pablok.kinopoisklight.network.dto
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class MovieResponse(
+    val docs: Array<Movie>
+)
+
+@Serializable
+data class Movie(
+    val id: Int,
+    val movieLength: Int,
+    val year: Int,
+    val name: String,
+    val description: String,
+    val poster: MoviePoster
+)
+
+@Serializable
+data class MoviePoster(
+    val url: String,
+    val previewUrl: String
+)
